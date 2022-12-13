d945678a1a46b72787b3a4fa9241e9fd442d663d
phamvinhquang97 <phamvinhquang77@gmail.com>
2022-12-13 16:34:57 +0930

    Create covid React project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import {MenuItem, FormControl, Select} from '@mui/material/';
+
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "INDIAN"]);
+
+
+  // STATE : how to write a variable in the REACT 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID-19 TRACKER</h1>
+      <div className="app_header">
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" value= "abc">
+          {/* Loop througt all the countries and show a drop down list of the options */}
+          {
+            countries.map(country => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+        </Select>
+      </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+      
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Grap */}
+
+      {/* Maps */}
     </div>
   );
 }
